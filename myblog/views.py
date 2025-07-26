7530a9b9226603e8eb6174f80259295f5bc1effe
김다온 <gimdaon@gimdaon-ui-MacBookAir.local>
2025-07-26 16:40:35 +0900

    Initial commit - django blog with comments

---
+++ myblog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'myblog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'myblog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
